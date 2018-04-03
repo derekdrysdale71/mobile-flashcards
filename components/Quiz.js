cc521f2b15d831d63ab8373719ec9f241025d76e
Derek <derek@darthvader.local>
2018-04-03 12:51:57 -0700

    add to quiz functionality

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { View, Text, StyleSheet, Alert } from 'react-native'
+import { View, Text, TouchableOpacity, Button, StyleSheet, Alert, Animated } from 'react-native'
 
 class Quiz extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -13,6 +13,17 @@ class Quiz extends Component {
     correctCount: 0
   }
 
+  onToggleCard = () => {
+    this.setState({ showAnswer: !this.state.showAnswer })
+  }
+
+  onRecordResult = isCorrect => {
+    this.setState({
+      index: this.state.index++,
+      correctCount: isCorrect && this.state.correctCount++,
+    })
+  }
+
   // Should probably break these out into individual components
   renderQuiz() {
     const { index, showAnswer } = this.state
@@ -21,28 +32,48 @@ class Quiz extends Component {
       <View style={styles.container}>
         <Text>Card {index + 1} / {questions.length}</Text>
         {!showAnswer ? this.renderQuestion() : this.renderAnswer()}
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button}>
+            <Text>Correct</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text>Incorrect</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 
   renderQuestion() {
-    const { index } = this.setState
-    const { questions } = this.props.deck
+    const { index } = this.state
+    const { questions } = this.props
+    console.log('Index:', index)
+    let question = questions[index]['question']
+    //const { question } = questions[index] || 'not set'
+    console.log('Questions:', questions)
+    console.log('Question:', question)
     return (
-      <View style={styles.container}>
+      <Animated.View style={styles.container}>
         <View>
-          <Text>{questions.question}</Text>
+          <Text style={styles.cardText}>{question}</Text>
         </View>
-        <Text>renderQuestion</Text>
-      </View>
+        <Button onPress={this.onToggleCard} title="Answer" />
+        <View>
+        </View>
+      </Animated.View>
     )
   }
 
   renderAnswer() {
+    const { index } = this.state
+    const answer = this.props.questions[index]['answer']
     return (
-      <View style={styles.container}>
-        <Text>renderAnswer</Text>
-      </View>
+      <Animated.View style={styles.container}>
+        <View>
+          <Text style={styles.cardText}>{answer}</Text>
+        </View>
+        <Button onPress={this.onToggleCard} title="Question" />
+      </Animated.View>
     )
   }
 
@@ -71,13 +102,23 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center'
   },
+  cardText: {
+    fontSize: 50
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#CCC8C8',
+    padding: 10,
+    borderRadius: 10,
+  }
 });
 
 const mapStateToProps = (state, { navigation }) => {
   const { title } = navigation.state.params
   return {
     title,
-    deck: state[title]
+    deck: state[title],
+    questions: state[title].questions
   }
 }
 
